f024901fa555e135bac990086b880d608e3f0a6c
reyhannaufal <reyhannaufal21@gmail.com>
2021-04-27 21:41:23 +0700

    feat: add login pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import IndexPage from './DashBoard';
+import SignIn from './SingIn';
 
 function App() {
   return (
-    <>
-      <IndexPage />
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/dashboard' component={IndexPage} />
+        <Route path='/' component={SignIn} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
