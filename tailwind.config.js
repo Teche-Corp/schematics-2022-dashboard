fe11ce758fe12a31c21c9b75275cf1cfb8fcbaee
rizqitsani <rizqitsani@gmail.com>
2021-05-28 22:21:46 +0700

    feat: add color scheme for sub event
    fix #6

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,30 @@ module.exports = {
     extend: {
       colors: {
         cyan: colors.cyan,
+        npc: {
+          DEFAULT: '#c93d45',
+          100: '#ffbbbc',
+          400: '#fd7d7e',
+          700: '#fd5457',
+        },
+        nlc: {
+          DEFAULT: '#fccc00',
+          100: '#fde466',
+          400: '#ffb349',
+          700: '#fe6021',
+        },
+        nst: {
+          DEFAULT: '#18a489',
+          100: '#95dc8e',
+          400: '#53c97f',
+          700: '#138176',
+        },
+        reeva: {
+          DEFAULT: '#008bd0',
+          100: '#a3e1fa',
+          400: '#77bede',
+          700: '#3d7196',
+        },
       },
     },
   },
