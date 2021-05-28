7cbea54a3ce1df4a5642af47c092840695976de0
Theodorus Clarence <theodorusclarence@gmail.com>
2021-05-28 11:17:30 +0700

    feat: add logo to header

--- src/components/SignInHeader.js
+++ src/components/SignInHeader.js
@@ -1,26 +1,16 @@
 import React from 'react';
 
-const SignInHeader = () => {
+const SignInHeader = ({ headerText }) => {
   return (
     <div className='sm:mx-auto sm:w-full sm:max-w-md'>
       <img
-        className='w-auto h-12 mx-auto'
-        src='https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg'
-        alt='Workflow'
+        className='w-32 mx-auto'
+        src='/images/logo/colored-no-title.png'
+        alt='Logo Colored'
       />
       <h2 className='mt-6 text-3xl font-extrabold text-center text-gray-900'>
-        Sign in to your account
+        {headerText}
       </h2>
-      <p className='mt-2 text-sm text-center text-gray-600 max-w'>
-        Or
-        {/* space */}
-        <a
-          href='/'
-          className='ml-1 font-medium text-indigo-600 hover:text-indigo-500'
-        >
-          start your 14-day free trial
-        </a>
-      </p>
     </div>
   );
 };
