49004ba3a21634dd243d1378a66a7242b65d399d
rizqitsani <rizqitsani@gmail.com>
2021-05-30 18:44:55 +0700

    feat: remove account section from edit profile

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -140,62 +140,6 @@ export default function EditProfile() {
                     </form>
                   </FormProvider>
                 </div>
-
-                <div className='mt-10 divide-y divide-gray-200'>
-                  <div className='space-y-1'>
-                    <h3 className='text-lg font-medium leading-6 text-gray-900'>
-                      Account
-                    </h3>
-                    <p className='max-w-2xl text-sm text-gray-500'>
-                      Manage how information is displayed on your account.
-                    </p>
-                  </div>
-                  <div className='mt-6'>
-                    <dl className='divide-y divide-gray-200'>
-                      <div className='py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4'>
-                        <dt className='text-sm font-medium text-gray-500'>
-                          Language
-                        </dt>
-                        <dd className='flex mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
-                          <span className='flex-grow'>English</span>
-                          <span className='flex-shrink-0 ml-4'>
-                            <button
-                              type='button'
-                              className='font-medium text-purple-600 bg-white rounded-md hover:text-purple-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500'
-                            >
-                              Update
-                            </button>
-                          </span>
-                        </dd>
-                      </div>
-                      <div className='py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:pt-5'>
-                        <dt className='text-sm font-medium text-gray-500'>
-                          Date format
-                        </dt>
-                        <dd className='flex mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
-                          <span className='flex-grow'>DD-MM-YYYY</span>
-                          <span className='flex items-start flex-shrink-0 ml-4 space-x-4'>
-                            <button
-                              type='button'
-                              className='font-medium text-purple-600 bg-white rounded-md hover:text-purple-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500'
-                            >
-                              Update
-                            </button>
-                            <span className='text-gray-300' aria-hidden='true'>
-                              |
-                            </span>
-                            <button
-                              type='button'
-                              className='font-medium text-purple-600 bg-white rounded-md hover:text-purple-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500'
-                            >
-                              Remove
-                            </button>
-                          </span>
-                        </dd>
-                      </div>
-                    </dl>
-                  </div>
-                </div>
               </div>
             </div>
           </div>
