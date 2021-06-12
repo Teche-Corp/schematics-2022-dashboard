d082a1303626fe6cc101bb9aa262d91538820789
bazoka-kaka <55269870+bazoka-kaka@users.noreply.github.com>
2021-06-12 19:53:42 +0700

    feat: add email validation npc junior

--- src/pages/CreateTeamNPCJunior.jsx
+++ src/pages/CreateTeamNPCJunior.jsx
@@ -91,7 +91,13 @@ export default function CreateTeamNPCJunior() {
                           label='Email'
                           id='member-email'
                           type='email'
-                          validation={{ required: 'Email is required' }}
+                          validation={{
+                            required: 'Email is required',
+                            pattern: {
+                              value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                              message: 'Please input correct email address',
+                            },
+                          }}
                         />
                       </div>
 
