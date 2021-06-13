ed021da9cc88b9910a20c5bfe25d3030ba7f3e02
rizqitsani <rizqitsani@gmail.com>
2021-06-13 21:17:53 +0700

    feat: change language to indonesian

--- src/pages/nlc/CreateTeam.jsx
+++ src/pages/nlc/CreateTeam.jsx
@@ -65,42 +65,48 @@ export default function CreateTeam() {
                 <div className='space-y-8 divide-y divide-gray-200'>
                   <div>
                     <h1 className='mb-6 text-4xl font-extrabold tracking-tight text-gray-900 sm:text-3xl md:text-4xl'>
-                      <span className='block xl:inline'>Create Team</span>{' '}
+                      <span className='block xl:inline'>Buat Tim</span>{' '}
                       <span className='block text-nlc xl:inline'>NLC</span>
                     </h1>
                     <div className='grid grid-cols-1 mt-6 gap-y-6 gap-x-4 sm:grid-cols-6'>
                       <div className='sm:col-span-4'>
                         <LightInput
-                          label='Team Name'
+                          label='Nama Tim'
                           id='team-name'
                           type='text'
-                          validation={{ required: 'Team name is required' }}
+                          validation={{
+                            required: 'Nama Tim tidak boleh kosong',
+                          }}
                         />
                       </div>
 
                       <div className='sm:col-span-4'>
                         <LightInput
-                          label='School Name'
+                          label='Asal Sekolah'
                           id='school-name'
                           type='text'
-                          validation={{ required: 'School name is required' }}
+                          validation={{
+                            required: 'Asal Sekolah tidak boleh kosong',
+                          }}
                         />
                       </div>
 
                       <div className='sm:col-span-3'>
                         <SelectCity
                           cities={cities}
-                          validation={{ required: 'City is required' }}
+                          validation={{ required: 'Kota tidak boleh kosong' }}
                           disabled={cities.length === 0}
                         />
                       </div>
 
                       <div className='sm:col-span-3'>
                         <LightInput
-                          label='Province'
+                          label='Provinsi'
                           id='province'
                           type='text'
-                          validation={{ required: 'Province is required' }}
+                          validation={{
+                            required: 'Provinsi tidak boleh kosong',
+                          }}
                           readOnly
                         />
                       </div>
@@ -110,7 +116,7 @@ export default function CreateTeam() {
                           label='Region'
                           id='region'
                           type='text'
-                          validation={{ required: 'Region is required' }}
+                          validation={{ required: 'Region tidak boleh kosong' }}
                           readOnly
                         />
                       </div>
@@ -119,15 +125,15 @@ export default function CreateTeam() {
 
                   <div className='pt-8'>
                     <h3 className='text-lg font-semibold leading-6 text-gray-900'>
-                      Team Leader Information
+                      Data Ketua Tim
                     </h3>
                     <div className='grid grid-cols-1 mt-6 gap-y-6 gap-x-4 sm:grid-cols-6'>
                       <div className='sm:col-span-4'>
                         <LightInput
-                          label='Name'
+                          label='Nama'
                           id='leader-name'
                           type='text'
-                          validation={{ required: 'Name is required' }}
+                          validation={{ required: 'Nama tidak boleh kosong' }}
                         />
                       </div>
 
@@ -137,10 +143,10 @@ export default function CreateTeam() {
                           id='leader-email'
                           type='email'
                           validation={{
-                            required: 'Email is required',
+                            required: 'Email tidak boleh kosong',
                             pattern: {
                               value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                              message: 'Please input correct email address',
+                              message: 'Email tidak valid',
                             },
                           }}
                         />
@@ -151,21 +157,21 @@ export default function CreateTeam() {
                           label='NISN'
                           id='leader-nisn'
                           type='text'
-                          validation={{ required: 'NISN is required' }}
+                          validation={{ required: 'NISN tidak boleh kosong' }}
                         />
                       </div>
 
                       <div className='sm:col-span-4'>
                         <LightInput
-                          label='Phone Number'
+                          label='Nomor Telepon'
                           id='leader-phone'
                           type='text'
                           validation={{
-                            required: 'Phone Number is required',
+                            required: 'Nomor Telepon tidak boleh kosong',
                             pattern: {
                               value: /^\+628[1-9][0-9]{8,11}$/,
                               message:
-                                'Please fill in phone number starting with +62 and correct format',
+                                'Nomor Telepon harus diawali +62 dan memiliki panjang 13-15 karakter',
                             },
                           }}
                         />
@@ -173,19 +179,23 @@ export default function CreateTeam() {
 
                       <div className='sm:col-span-2'>
                         <LightInput
-                          label='Line ID'
+                          label='ID Line'
                           id='leader-line'
                           type='text'
-                          validation={{ required: 'Line ID is required' }}
+                          validation={{
+                            required: 'ID Line tidak boleh kosong',
+                          }}
                         />
                       </div>
 
                       <div className='sm:col-span-6'>
                         <LightInput
-                          label='Address'
+                          label='Alamat'
                           id='leader-address'
                           type='text'
-                          validation={{ required: 'Address is required' }}
+                          validation={{
+                            required: 'Alamat tidak boleh kosong',
+                          }}
                         />
                       </div>
 
@@ -194,7 +204,7 @@ export default function CreateTeam() {
                           htmlFor='cover_photo'
                           className='block text-sm font-normal text-gray-700'
                         >
-                          Student ID Card
+                          Foto Kartu Pelajar
                         </label>
                         <div className='flex justify-center px-6 pt-5 pb-6 mt-1 border-2 border-gray-300 border-dashed rounded-md'>
                           <div className='space-y-1 text-center'>
@@ -238,15 +248,15 @@ export default function CreateTeam() {
 
                   <div className='pt-8'>
                     <h3 className='text-lg font-semibold leading-6 text-gray-900'>
-                      Team Member Information
+                      Data Anggota
                     </h3>
                     <div className='grid grid-cols-1 mt-6 gap-y-6 gap-x-4 sm:grid-cols-6'>
                       <div className='sm:col-span-4'>
                         <LightInput
-                          label='Name'
+                          label='Nama'
                           id='member-name'
                           type='text'
-                          validation={{ required: 'Name is required' }}
+                          validation={{ required: 'Nama tidak boleh kosong' }}
                         />
                       </div>
 
@@ -256,10 +266,10 @@ export default function CreateTeam() {
                           id='member-email'
                           type='email'
                           validation={{
-                            required: 'Email is required',
+                            required: 'Email tidak boleh kosong',
                             pattern: {
                               value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                              message: 'Please input correct email address',
+                              message: 'Email tidak valid',
                             },
                           }}
                         />
@@ -270,21 +280,21 @@ export default function CreateTeam() {
                           label='NISN'
                           id='member-nisn'
                           type='text'
-                          validation={{ required: 'NISN is required' }}
+                          validation={{ required: 'NISN tidak boleh kosong' }}
                         />
                       </div>
 
                       <div className='sm:col-span-4'>
                         <LightInput
-                          label='Phone Number'
+                          label='Nomor Telepon'
                           id='member-phone'
                           type='text'
                           validation={{
-                            required: 'Phone Number is required',
+                            required: 'Nomor Telepon tidak boleh kosong',
                             pattern: {
                               value: /^\+628[1-9][0-9]{8,11}$/,
                               message:
-                                'Please fill in phone number starting with +62 and correct format',
+                                'Nomor Telepon harus diawali +62 dan memiliki panjang 13-15 karakter',
                             },
                           }}
                         />
@@ -292,19 +302,23 @@ export default function CreateTeam() {
 
                       <div className='sm:col-span-2'>
                         <LightInput
-                          label='Line ID'
+                          label='ID Line'
                           id='member-line'
                           type='text'
-                          validation={{ required: 'Line ID is required' }}
+                          validation={{
+                            required: 'ID Line tidak boleh kosong',
+                          }}
                         />
                       </div>
 
                       <div className='sm:col-span-6'>
                         <LightInput
-                          label='Address'
+                          label='Alamat'
                           id='member-address'
                           type='text'
-                          validation={{ required: 'Address is required' }}
+                          validation={{
+                            required: 'Alamat tidak boleh kosong',
+                          }}
                         />
                       </div>
 
@@ -313,7 +327,7 @@ export default function CreateTeam() {
                           htmlFor='cover_photo'
                           className='block text-sm font-normal text-gray-700'
                         >
-                          Student ID Card
+                          Foto Kartu Pelajar
                         </label>
                         <div className='flex justify-center px-6 pt-5 pb-6 mt-1 border-2 border-gray-300 border-dashed rounded-md'>
                           <div className='space-y-1 text-center'>
@@ -362,13 +376,13 @@ export default function CreateTeam() {
                       to='/sch-nlc'
                       className='px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-dark-400'
                     >
-                      Cancel
+                      Kembali
                     </Link>
                     <button
                       type='submit'
                       className='inline-flex justify-center px-4 py-2 ml-3 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-nlc hover:bg-nlc-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-nlc-400'
                     >
-                      Save
+                      Buat Tim
                     </button>
                   </div>
                 </div>
