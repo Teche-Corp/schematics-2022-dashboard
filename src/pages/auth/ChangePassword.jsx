d40b4431ac8dbce0297c17f9b0a57ec3a31045d7
Theodorus Clarence <theodorusclarence@gmail.com>
2021-06-13 20:46:42 +0700

    feat: implements change password

--- src/pages/auth/ChangePassword.jsx
+++ src/pages/auth/ChangePassword.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import { useState } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
 
 import AuthHeader from '@/components/AuthHeader';
 import PasswordInput from '@/components/PasswordInput';
+import SubmitButton from '@/components/SubmitButton';
+import Input from '@/components/Input';
 
 export default function ChangePassword() {
   const methods = useForm();
+  const history = useHistory();
   const { handleSubmit } = methods;
 
-  const handleChangePassword = (data) => {
-    console.log(data);
+  const [loading, setLoading] = useState(false);
+
+  const handleChangePassword = async (data) => {
+    try {
+      setLoading(true);
+      await axios.post('/user/change-password', { ...data });
+
+      setLoading(false);
+      toast.success(
+        `Password changed successfully, you can sign in with your new password`,
+      );
+      history.replace('/signin');
+    } catch (err) {
+      console.error(err);
+      toast.error('Uh oh! Something is wrong, please try again');
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -24,21 +46,33 @@ export default function ChangePassword() {
                   className='space-y-6'
                   onSubmit={handleSubmit(handleChangePassword)}
                 >
+                  <Input
+                    label='Email'
+                    id='email'
+                    type='email'
+                    validation={{ required: 'Email is required' }}
+                  />
+
                   <PasswordInput
-                    label='Password'
-                    id='password'
+                    label='Old Password'
+                    id='old_password'
                     validation={{
-                      required: 'Password is required',
+                      required: 'Old Password is required',
+                    }}
+                  />
+
+                  <PasswordInput
+                    label='New Password'
+                    id='new_password'
+                    validation={{
+                      required: 'New Password is required',
                     }}
                   />
 
                   <div>
-                    <button
-                      className='flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-                      type='submit'
-                    >
+                    <SubmitButton loading={loading}>
                       Change Password
-                    </button>
+                    </SubmitButton>
                   </div>
                 </form>
               </FormProvider>
