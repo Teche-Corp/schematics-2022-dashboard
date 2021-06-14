62388674282b96470327d8a1d0cf5f65aaa5048f
rizqitsani <rizqitsani@gmail.com>
2021-06-14 13:50:00 +0700

    feat: use DragnDropInput in create team nlc

--- src/pages/nlc/CreateTeam.jsx
+++ src/pages/nlc/CreateTeam.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import DashboardShell from '@/layout/DashboardShell';
 import LightInput from '@/components/LightInput';
 import SelectCity from '@/components/SelectCity';
+import DragnDropInput from '@/components/DragnDropInput';
 
 export default function CreateTeam() {
   const [cities, setCities] = useState([]);
@@ -200,48 +201,15 @@ export default function CreateTeam() {
                       </div>
 
                       <div className='sm:col-span-6'>
-                        <label
-                          htmlFor='cover_photo'
-                          className='block text-sm font-normal text-gray-700'
-                        >
-                          Foto Kartu Pelajar
-                        </label>
-                        <div className='flex justify-center px-6 pt-5 pb-6 mt-1 border-2 border-gray-300 border-dashed rounded-md'>
-                          <div className='space-y-1 text-center'>
-                            <svg
-                              className='w-12 h-12 mx-auto text-gray-400'
-                              stroke='currentColor'
-                              fill='none'
-                              viewBox='0 0 48 48'
-                              aria-hidden='true'
-                            >
-                              <path
-                                d='M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02'
-                                strokeWidth={2}
-                                strokeLinecap='round'
-                                strokeLinejoin='round'
-                              />
-                            </svg>
-                            <div className='flex text-sm text-gray-600'>
-                              <label
-                                htmlFor='file-upload'
-                                className='relative font-medium bg-white rounded-md cursor-pointer text-nlc hover:text-nlc-400 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-nlc-400'
-                              >
-                                <span>Upload a file</span>
-                                <input
-                                  id='file-upload'
-                                  name='file-upload'
-                                  type='file'
-                                  className='sr-only'
-                                />
-                              </label>
-                              <p className='pl-1'>or drag and drop</p>
-                            </div>
-                            <p className='text-xs text-gray-500'>
-                              PNG, JPG, GIF up to 10MB
-                            </p>
-                          </div>
-                        </div>
+                        <DragnDropInput
+                          label='Foto Kartu Pelajar'
+                          id='leader-id'
+                          accept='image/png, image/jpg, image/jpeg, image/gif'
+                          maxFiles={1}
+                          validation={{
+                            required: 'Foto Kartu Pelajar tidak boleh kosong',
+                          }}
+                        />
                       </div>
                     </div>
                   </div>
@@ -323,48 +291,15 @@ export default function CreateTeam() {
                       </div>
 
                       <div className='sm:col-span-6'>
-                        <label
-                          htmlFor='cover_photo'
-                          className='block text-sm font-normal text-gray-700'
-                        >
-                          Foto Kartu Pelajar
-                        </label>
-                        <div className='flex justify-center px-6 pt-5 pb-6 mt-1 border-2 border-gray-300 border-dashed rounded-md'>
-                          <div className='space-y-1 text-center'>
-                            <svg
-                              className='w-12 h-12 mx-auto text-gray-400'
-                              stroke='currentColor'
-                              fill='none'
-                              viewBox='0 0 48 48'
-                              aria-hidden='true'
-                            >
-                              <path
-                                d='M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02'
-                                strokeWidth={2}
-                                strokeLinecap='round'
-                                strokeLinejoin='round'
-                              />
-                            </svg>
-                            <div className='flex text-sm text-gray-600'>
-                              <label
-                                htmlFor='file-upload'
-                                className='relative font-medium bg-white rounded-md cursor-pointer text-nlc hover:text-nlc-400 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-nlc-400'
-                              >
-                                <span>Upload a file</span>
-                                <input
-                                  id='file-upload'
-                                  name='file-upload'
-                                  type='file'
-                                  className='sr-only'
-                                />
-                              </label>
-                              <p className='pl-1'>or drag and drop</p>
-                            </div>
-                            <p className='text-xs text-gray-500'>
-                              PNG, JPG, GIF up to 10MB
-                            </p>
-                          </div>
-                        </div>
+                        <DragnDropInput
+                          label='Foto Kartu Pelajar'
+                          id='member-id'
+                          accept='image/png, image/jpg, image/jpeg, image/gif'
+                          maxFiles={1}
+                          validation={{
+                            required: 'Foto Kartu Pelajar tidak boleh kosong',
+                          }}
+                        />
                       </div>
                     </div>
                   </div>
