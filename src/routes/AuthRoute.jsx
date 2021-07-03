ca8ed8b85afc5f458108f2ec81ab6acd6da7ee14
rizqitsani <rizqitsani@gmail.com>
2021-07-03 10:45:08 +0700

    feat: add `AuthRoute` component

---
+++ src/routes/AuthRoute.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { Redirect, Route } from 'react-router-dom';
+import { ImSpinner8 } from 'react-icons/im';
+import { useAuthState } from '@/contexts/AuthContext';
+
+const AuthRoute = ({ component: Component, ...rest }) => {
+  const { authenticated, loading } = useAuthState();
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        loading ? (
+          <div className='flex flex-col items-center justify-center min-h-screen bg-dark'>
+            <ImSpinner8 className='mb-2 text-4xl text-light animate-spin' />
+            <p>Loading...</p>
+          </div>
+        ) : authenticated ? (
+          <Redirect to={'/my'} />
+        ) : (
+          <Component {...props} />
+        )
+      }
+    />
+  );
+};
+
+export default AuthRoute;
