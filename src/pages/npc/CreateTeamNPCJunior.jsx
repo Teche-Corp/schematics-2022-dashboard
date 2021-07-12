96b740fc744a92c8f3a7ff990a98f0597f9863a6
bazoka-kaka <55269870+bazoka-kaka@users.noreply.github.com>
2021-07-13 03:05:49 +0700

    feat: add team details on create team npc junior

--- src/pages/npc/CreateTeamNPCJunior.jsx
+++ src/pages/npc/CreateTeamNPCJunior.jsx
@@ -1,16 +1,100 @@
-import { FormProvider, useForm } from 'react-hook-form';
+import { useEffect } from 'react';
+import { useHistory } from 'react-router';
+import { FormProvider, useForm, useWatch } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import toast from 'react-hot-toast';
+import axios from 'axios';
+import useSWR from 'swr';
+
+import { useAuthDispatch, useAuthState } from '@/contexts/AuthContext';
 
 import DashboardShell from '@/layout/DashboardShell';
 import LightInput from '@/components/LightInput';
+import SelectCity from '@/components/SelectCity';
+import DragnDropInput from '@/components/DragnDropInput';
 
-export default function CreateTeamNPCJunior() {
+import { bearerToken, classNames } from '@/lib/helper';
+import useLoadingToast from '@/hooks/useLoadingToast';
+
+export default function CreateTeam() {
+  const history = useHistory();
+  const isLoading = useLoadingToast();
+
   const methods = useForm();
-  const { handleSubmit } = methods;
+  const { control, handleSubmit, setValue } = methods;
 
-  const handleCreateTeam = (data) => {
-    console.log(data);
+  const { data, error: fetchError } = useSWR('/region/list');
+  const cities = data?.data;
+
+  const cityValue = useWatch({
+    control,
+    name: 'city',
+  });
+
+  const { user } = useAuthState();
+  const dispatch = useAuthDispatch();
+
+  useEffect(() => {
+    if (cityValue !== undefined) {
+      const id = cityValue?.value;
+      const city = cities.find((city) => city.id === id);
+      setValue('province', city?.province_name, {
+        shouldValidate: true,
+        shouldDirty: true,
+      });
+      setValue('region', city?.region_name, {
+        shouldValidate: true,
+        shouldDirty: true,
+      });
+    }
+  }, [cityValue, cities, setValue]);
+
+  const handleCreateTeam = async (data) => {
+    const formData = new FormData();
+
+    const newBody = {
+      kota_id: data.city.value,
+      ketua_nisn: data['leader-nisn'],
+      ketua_alamat: data['leader-address'],
+      ketua_id_line: data['leader-line'],
+      status_id: 2,
+      team_name: data['team-name'],
+      team_password: `schnpc${user.name}`,
+      team_institusi: data['school-name'],
+      kp_ketua: data['leader-id'][0],
+      'anggota[0][name]': data['member-name'],
+      'anggota[0][email]': data['member-email'],
+      'anggota[0][nisn]': data['member-nisn'],
+      'anggota[0][phone]': data['member-phone'],
+      'anggota[0][alamat]': data['member-address'],
+      'anggota[0][id_line]': data['member-line'],
+      'anggota[0][kp_anggota]': data['member-id'][0],
+    };
+
+    for (let key in newBody) {
+      formData.append(key, newBody[key]);
+    }
+
+    toast.promise(
+      axios
+        .post('/npc/team/create/junior', formData, {
+          headers: { ...bearerToken(), 'Content-Type': 'multipart/form-data' },
+        })
+        .then((res) => {
+          dispatch('ASSIGN_NLC', res.data.data);
+          history.push('/my/sch-npc/team/junior');
+        }),
+      {
+        loading: 'Loading...',
+        success: 'Berhasil membuat tim!',
+        error: (err) => err.response.data.msg,
+      },
+    );
   };
+
+  if (fetchError) {
+    return toast.error('Gagal mengambil data kota.');
+  }
 
   return (
     <DashboardShell>
@@ -28,45 +112,61 @@ export default function CreateTeamNPCJunior() {
                 <div className='space-y-8 divide-y divide-gray-200'>
                   <div>
                     <h1 className='mb-6 text-4xl font-extrabold tracking-tight text-gray-900 sm:text-3xl md:text-4xl'>
-                      <span className='block xl:inline'>Create Team</span>{' '}
-                      <span className='block text-npc-400 xl:inline'>
-                        Junior NPC
+                      <span className='block xl:inline'>Buat Tim</span>{' '}
+                      <span className='block text-red-400 xl:inline'>
+                        NPC Junior
                       </span>
                     </h1>
                     <div className='grid grid-cols-1 mt-6 gap-y-6 gap-x-4 sm:grid-cols-6'>
                       <div className='sm:col-span-4'>
                         <LightInput
-                          label='Team Name'
+                          label='Nama Tim'
                           id='team-name'
                           type='text'
-                          validation={{ required: 'Team name is required' }}
+                          validation={{
+                            required: 'Nama Tim tidak boleh kosong',
+                          }}
                         />
                       </div>
 
                       <div className='sm:col-span-4'>
                         <LightInput
-                          label='School Name'
+                          label='Asal Sekolah'
                           id='school-name'
                           type='text'
-                          validation={{ required: 'School name is required' }}
+                          validation={{
+                            required: 'Asal Sekolah tidak boleh kosong',
+                          }}
+                        />
+                      </div>
+
+                      <div className='sm:col-span-3'>
+                        <SelectCity
+                          cities={cities || []}
+                          validation={{ required: 'Kota tidak boleh kosong' }}
+                          disabled={!cities}
                         />
                       </div>
 
                       <div className='sm:col-span-3'>
                         <LightInput
-                          label='City'
-                          id='city'
-                          type='text'
-                          validation={{ required: 'City is required' }}
-                        />
-                      </div>
-
-                      <div className='sm:col-span-3'>
-                        <LightInput
-                          label='Province'
+                          label='Provinsi'
                           id='province'
                           type='text'
-                          validation={{ required: 'Province is required' }}
+                          validation={{
+                            required: 'Provinsi tidak boleh kosong',
+                          }}
+                          readOnly
+                        />
+                      </div>
+
+                      <div className='sm:col-span-3'>
+                        <LightInput
+                          label='Region'
+                          id='region'
+                          type='text'
+                          validation={{ required: 'Region tidak boleh kosong' }}
+                          readOnly
                         />
                       </div>
                     </div>
@@ -74,15 +174,15 @@ export default function CreateTeamNPCJunior() {
 
                   <div className='pt-8'>
                     <h3 className='text-lg font-semibold leading-6 text-gray-900'>
-                      Team Member Information
+                      Data Anggota
                     </h3>
                     <div className='grid grid-cols-1 mt-6 gap-y-6 gap-x-4 sm:grid-cols-6'>
                       <div className='sm:col-span-4'>
                         <LightInput
-                          label='Name'
+                          label='Nama'
                           id='member-name'
                           type='text'
-                          validation={{ required: 'Name is required' }}
+                          validation={{ required: 'Nama tidak boleh kosong' }}
                         />
                       </div>
 
@@ -92,101 +192,72 @@ export default function CreateTeamNPCJunior() {
                           id='member-email'
                           type='email'
                           validation={{
-                            required: 'Email is required',
+                            required: 'Email tidak boleh kosong',
                             pattern: {
                               value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                              message: 'Please input correct email address',
+                              message: 'Email tidak valid',
                             },
                           }}
                         />
                       </div>
 
-                      <div className='sm:col-span-4'>
+                      <div className='sm:col-span-3'>
                         <LightInput
                           label='NISN'
-                          id='leader-nisn'
+                          id='member-nisn'
                           type='text'
-                          validation={{ required: 'NISN is required' }}
+                          validation={{ required: 'NISN tidak boleh kosong' }}
                         />
                       </div>
 
                       <div className='sm:col-span-4'>
                         <LightInput
-                          label='Phone Number'
-                          id='leader-phone'
+                          label='Nomor Telepon'
+                          id='member-phone'
                           type='text'
+                          placeholder='+6281234567890'
+                          helperText='Nomor Telepon diawali +62'
                           validation={{
-                            required: 'Phone Number is required',
+                            required: 'Nomor Telepon tidak boleh kosong',
                             pattern: {
                               value: /^\+628[1-9][0-9]{8,11}$/,
                               message:
-                                'Please fill in phone number starting with +62 and correct format',
+                                'Nomor Telepon harus diawali +62 dan memiliki panjang 13-15 karakter',
                             },
                           }}
                         />
                       </div>
 
-                      {/* <div className='sm:col-span-2'>
+                      <div className='sm:col-span-2'>
                         <LightInput
-                          label='Line ID'
-                          id='leader-line'
+                          label='ID Line (Opsional)'
+                          id='member-line'
                           type='text'
-                          validation={{ required: 'Line ID is required' }}
-                        />
-                      </div> */}
-
-                      <div className='sm:col-span-6'>
-                        <LightInput
-                          label='Address'
-                          id='leader-address'
-                          type='text'
-                          validation={{ required: 'Address is required' }}
                         />
                       </div>
 
                       <div className='sm:col-span-6'>
-                        <label
-                          htmlFor='cover_photo'
-                          className='block text-sm font-normal text-gray-700'
-                        >
-                          Student ID Card
-                        </label>
-                        <div className='flex justify-center px-6 pt-5 pb-6 mt-1 border-2 border-gray-300 border-dashed rounded-md'>
-                          <div className='space-y-1 text-center'>
-                            <svg
-                              className='w-12 h-12 mx-auto text-gray-400'
-                              stroke='currentColor'
-                              fill='none'
-                              viewBox='0 0 48 48'
-                              aria-hidden='true'
-                            >
-                              <path
-                                d='M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02'
-                                strokeWidth={2}
-                                strokeLinecap='round'
-                                strokeLinejoin='round'
-                              />
-                            </svg>
-                            <div className='flex text-sm text-gray-600'>
-                              <label
-                                htmlFor='file-upload'
-                                className='relative font-medium bg-white rounded-md cursor-pointer text-npc-400 hover:text-npc-700 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-npc-400'
-                              >
-                                <span>Upload a file</span>
-                                <input
-                                  id='file-upload'
-                                  name='file-upload'
-                                  type='file'
-                                  className='sr-only'
-                                />
-                              </label>
-                              <p className='pl-1'>or drag and drop</p>
-                            </div>
-                            <p className='text-xs text-gray-500'>
-                              PNG, JPG, GIF up to 10MB
-                            </p>
-                          </div>
-                        </div>
+                        <LightInput
+                          label='Alamat'
+                          id='member-address'
+                          type='text'
+                          validation={{
+                            required: 'Alamat tidak boleh kosong',
+                          }}
+                        />
+                      </div>
+
+                      <div className='sm:col-span-6'>
+                        <DragnDropInput
+                          label='Foto Kartu Pelajar/Surat Keterangan Siswa Aktif'
+                          id='member-id'
+                          accept='image/png, image/jpg, image/jpeg'
+                          helperText='File dalam format jpg, png, atau jpeg'
+                          maxFiles={1}
+                          validation={{
+                            required: 'Foto Kartu Pelajar tidak boleh kosong',
+                          }}
+                        />
                       </div>
                     </div>
                   </div>
@@ -198,13 +269,17 @@ export default function CreateTeamNPCJunior() {
                       to='/my/sch-npc/team'
                       className='px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-dark-400'
                     >
-                      Cancel
+                      Kembali
                     </Link>
                     <button
                       type='submit'
-                      className='inline-flex justify-center px-4 py-2 ml-3 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-npc-400 hover:bg-npc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-npc-400'
+                      disabled={isLoading}
+                      className={classNames(
+                        'inline-flex justify-center px-4 py-2 ml-3 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-npc-400 hover:bg-npc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-npc-400',
+                        isLoading && 'filter brightness-90 cursor-wait',
+                      )}
                     >
-                      Save
+                      Buat Tim
                     </button>
                   </div>
                 </div>
