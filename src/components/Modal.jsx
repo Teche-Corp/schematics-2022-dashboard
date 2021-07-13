aa4c36cc6531278b9cb619f283cccebd34077312
reyhannaufal <reyhannaufal21@gmail.com>
2021-07-13 13:00:39 +0800

    feat: edit npc modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -16,7 +16,7 @@ export default function Modal({ open, setOpen }) {
         open={open}
         onClose={setOpen}
       >
-        <div className='flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0'>
+        <div className='flex items-end justify-center px-4 pt-4 pb-20 text-center h-1/2 sm:block sm:p-0'>
           <Transition.Child
             as={Fragment}
             enter='ease-out duration-300'
@@ -45,44 +45,33 @@ export default function Modal({ open, setOpen }) {
             leaveFrom='opacity-100 translate-y-0 sm:scale-100'
             leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
           >
-            <div className='inline-block overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full'>
+            <div className='inline-block w-3/4 overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full'>
               <div className='px-4 pt-5 pb-4 bg-white sm:p-6 sm:pb-4'>
-                <div className='sm:flex sm:items-start'>
-                  <div className='flex items-center justify-center flex-shrink-0 w-12 h-12 mx-auto bg-red-100 rounded-full sm:mx-0 sm:h-10 sm:w-10'>
-                    <HiDesktopComputer
-                      className='w-6 h-6 text-red-600'
-                      aria-hidden='true'
-                    />
-                  </div>
-                  <div className='mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left'>
+                <div className='justify-center sm:flex sm:items-start'>
+                  <div className='mt-3 sm:mt-0 sm:ml-4'>
                     <Dialog.Title
                       as='h3'
-                      className='text-lg font-medium leading-6 text-gray-900'
+                      className='text-2xl font-bold leading-6 text-center text-gray-900 '
                     >
-                      Schematics NPC
+                      <span className='text-npc'>Pilih</span> kategori Tim
                     </Dialog.Title>
-                    <div className='mt-2'>
-                      <p className='text-sm text-gray-500'>
-                        Pilihlah kategori team anda.
-                      </p>
-                    </div>
                   </div>
                 </div>
               </div>
-              <div className='px-4 py-3 bg-gray-50 sm:px-6 sm:flex sm:flex-row-reverse'>
+              <div className='justify-center px-4 py-3 bg-white sm:px-6 sm:flex sm:flex-row-reverse'>
                 <Link
                   type='button'
-                  className='inline-flex justify-center w-full px-4 py-2 text-base font-medium text-white bg-red-600 border border-transparent rounded-md shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm'
+                  className='inline-flex justify-center w-full px-4 py-2 mt-3 text-base font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm'
                   to='/my/sch-npc/team/create/senior'
                 >
-                  Senior NPC
+                  Senior
                 </Link>
                 <Link
                   type='button'
-                  className='inline-flex justify-center w-full px-4 py-2 mt-3 text-base font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm'
+                  className='inline-flex justify-center w-full px-4 py-2 mt-3 text-base font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm'
                   to='/my/sch-npc/team/create/junior'
                 >
-                  Junior NPC
+                  Junior
                 </Link>
               </div>
             </div>
