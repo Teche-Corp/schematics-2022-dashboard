7e6dfa32439bed3e1aff09dd99d9762fc06037d8
christhopermarcelino <christhopermarcelino@gmail.com>
2021-07-13 13:39:59 +0700

    feat: use DragnDropInput in create team npc senior

--- src/pages/npc/CreateTeamNPCSenior.jsx
+++ src/pages/npc/CreateTeamNPCSenior.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 import DashboardShell from '@/layout/DashboardShell';
 import LightInput from '@/components/LightInput';
+import DragnDropInput from '@/components/DragnDropInput';
 
 export default function CreateTeamNPCSenior() {
   const methods = useForm();
@@ -147,48 +148,17 @@ export default function CreateTeamNPCSenior() {
                       </div>
 
                       <div className='sm:col-span-6'>
-                        <label
-                          htmlFor='cover_photo'
-                          className='block text-sm font-normal text-gray-700'
-                        >
-                          Student ID Card
-                        </label>
-                        <div className='flex justify-center px-6 pt-5 pb-6 mt-1 border-2 border-gray-300 border-dashed rounded-md'>
-                          <div className='space-y-1 text-center'>
-                            <svg
-                              className='w-12 h-12 mx-auto text-gray-400'
-                              stroke='currentColor'
-                              fill='none'
-                              viewBox='0 0 48 48'
-                              aria-hidden='true'
-                            >
-                              <path
-                                d='M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02'
-                                strokeWidth={2}
-                                strokeLinecap='round'
-                                strokeLinejoin='round'
-                              />
-                            </svg>
-                            <div className='flex text-sm text-gray-600'>
-                              <label
-                                htmlFor='file-upload-leader'
-                                className='relative font-medium bg-white rounded-md cursor-pointer text-npc-400 hover:text-npc-700 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-npc-400'
-                              >
-                                <span>Upload a file</span>
-                                <input
-                                  id='file-upload-leader'
-                                  name='leader-student-id'
-                                  type='file'
-                                  className='sr-only'
-                                />
-                              </label>
-                              <p className='pl-1'>or drag and drop</p>
-                            </div>
-                            <p className='text-xs text-gray-500'>
-                              PNG, JPG, GIF up to 10MB
-                            </p>
-                          </div>
-                        </div>
+                        <DragnDropInput
+                          label='Foto Kartu Tanda Mahasiswa/Surat Keterangan Mahasiswa Aktif'
+                          id='leader-student-id'
+                          accept='image/png, image/jpg, image/jpeg'
+                          helperText='File dalam format jpg, png, atau jpeg'
+                          maxFiles={1}
+                          validation={{
+                            required:
+                              'Foto Kartu Tanda Mahasiswa tidak boleh kosong',
+                          }}
+                        />
                       </div>
                     </div>
                   </div>
@@ -266,48 +236,17 @@ export default function CreateTeamNPCSenior() {
                       </div>
 
                       <div className='sm:col-span-6'>
-                        <label
-                          htmlFor='cover_photo'
-                          className='block text-sm font-normal text-gray-700'
-                        >
-                          Student ID Card
-                        </label>
-                        <div className='flex justify-center px-6 pt-5 pb-6 mt-1 border-2 border-gray-300 border-dashed rounded-md'>
-                          <div className='space-y-1 text-center'>
-                            <svg
-                              className='w-12 h-12 mx-auto text-gray-400'
-                              stroke='currentColor'
-                              fill='none'
-                              viewBox='0 0 48 48'
-                              aria-hidden='true'
-                            >
-                              <path
-                                d='M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02'
-                                strokeWidth={2}
-                                strokeLinecap='round'
-                                strokeLinejoin='round'
-                              />
-                            </svg>
-                            <div className='flex text-sm text-gray-600'>
-                              <label
-                                htmlFor='file-upload-member-1'
-                                className='relative font-medium bg-white rounded-md cursor-pointer text-npc-400 hover:text-npc-700 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-npc-400'
-                              >
-                                <span>Upload a file</span>
-                                <input
-                                  id='file-upload-member-1'
-                                  name='member1-student-id'
-                                  type='file'
-                                  className='sr-only'
-                                />
-                              </label>
-                              <p className='pl-1'>or drag and drop</p>
-                            </div>
-                            <p className='text-xs text-gray-500'>
-                              PNG, JPG, GIF up to 10MB
-                            </p>
-                          </div>
-                        </div>
+                        <DragnDropInput
+                          label='Foto Kartu Tanda Mahasiswa/Surat Keterangan Mahasiswa Aktif'
+                          id='member1-student-id'
+                          accept='image/png, image/jpg, image/jpeg'
+                          helperText='File dalam format jpg, png, atau jpeg'
+                          maxFiles={1}
+                          validation={{
+                            required:
+                              'Foto Kartu Tanda Mahasiswa tidak boleh kosong',
+                          }}
+                        />
                       </div>
                     </div>
                   </div>
@@ -385,48 +324,17 @@ export default function CreateTeamNPCSenior() {
                       </div>
 
                       <div className='sm:col-span-6'>
-                        <label
-                          htmlFor='cover_photo'
-                          className='block text-sm font-normal text-gray-700'
-                        >
-                          Student ID Card
-                        </label>
-                        <div className='flex justify-center px-6 pt-5 pb-6 mt-1 border-2 border-gray-300 border-dashed rounded-md'>
-                          <div className='space-y-1 text-center'>
-                            <svg
-                              className='w-12 h-12 mx-auto text-gray-400'
-                              stroke='currentColor'
-                              fill='none'
-                              viewBox='0 0 48 48'
-                              aria-hidden='true'
-                            >
-                              <path
-                                d='M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02'
-                                strokeWidth={2}
-                                strokeLinecap='round'
-                                strokeLinejoin='round'
-                              />
-                            </svg>
-                            <div className='flex text-sm text-gray-600'>
-                              <label
-                                htmlFor='file-upload-member-2'
-                                className='relative font-medium bg-white rounded-md cursor-pointer text-npc-400 hover:text-npc-700 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-npc-400'
-                              >
-                                <span>Upload a file</span>
-                                <input
-                                  id='file-upload-member-2'
-                                  name='member2-student-id'
-                                  type='file'
-                                  className='sr-only'
-                                />
-                              </label>
-                              <p className='pl-1'>or drag and drop</p>
-                            </div>
-                            <p className='text-xs text-gray-500'>
-                              PNG, JPG, GIF up to 10MB
-                            </p>
-                          </div>
-                        </div>
+                        <DragnDropInput
+                          label='Foto Kartu Tanda Mahasiswa/Surat Keterangan Mahasiswa Aktif'
+                          id='member2-student-id'
+                          accept='image/png, image/jpg, image/jpeg'
+                          helperText='File dalam format jpg, png, atau jpeg'
+                          maxFiles={1}
+                          validation={{
+                            required:
+                              'Foto Kartu Tanda Mahasiswa tidak boleh kosong',
+                          }}
+                        />
                       </div>
                     </div>
                   </div>
