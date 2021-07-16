cd6ce1eee96ded137c7b803752f5a1569871e7fb
rizqitsani <rizqitsani@gmail.com>
2021-07-16 23:54:18 +0700

    feat: change method for getting teamId
    use includes because npc event consist of npc_junior and npc_senior

--- src/hooks/useTeamId.jsx
+++ src/hooks/useTeamId.jsx
@@ -2,8 +2,8 @@ import { useAuthState } from '@/contexts/AuthContext';
 
 export default function useTeamId(eventName) {
   const { user } = useAuthState();
-  const teamId = user?.team.find(
-    (teamElement) => teamElement.event === eventName,
+  const teamId = user?.team.find((teamElement) =>
+    teamElement.event.includes(eventName),
   )?.team_id;
   return teamId;
 }
