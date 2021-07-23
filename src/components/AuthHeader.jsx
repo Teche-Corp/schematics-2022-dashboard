bad6dcaa5f101cb87c3f628071fff364ddf8c922
rizqitsani <rizqitsani@gmail.com>
2021-07-23 16:17:15 +0700

    feat: add link from `AuthHeader` to landing page

--- src/components/AuthHeader.jsx
+++ src/components/AuthHeader.jsx
@@ -1,11 +1,15 @@
+import UnstyledLink from './UnstyledLink';
+
 export default function AuthHeader({ headerText }) {
   return (
     <div className='sm:mx-auto sm:w-full sm:max-w-md'>
-      <img
-        className='w-1/2 mx-auto'
-        src={`${process.env.PUBLIC_URL}/images/logo/colored-title.png`}
-        alt='Logo Colored'
-      />
+      <UnstyledLink href='https://schematics.its.ac.id/'>
+        <img
+          className='w-1/2 mx-auto'
+          src={`${process.env.PUBLIC_URL}/images/logo/colored-title.png`}
+          alt='Logo Colored'
+        />
+      </UnstyledLink>
       <h1 className='mt-6 text-4xl font-extrabold text-center text-white'>
         {headerText}
       </h1>
