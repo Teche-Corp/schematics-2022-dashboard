b0e2e25b38f8cec6da37cddd32f1d6cf1e6204e0
Theodorus Clarence <theodorusclarence@gmail.com>
2021-07-24 21:41:07 +0700

    fix(auth): make auth header logo open new tab

--- src/components/AuthHeader.jsx
+++ src/components/AuthHeader.jsx
@@ -3,7 +3,7 @@ import UnstyledLink from './UnstyledLink';
 export default function AuthHeader({ headerText }) {
   return (
     <div className='sm:mx-auto sm:w-full sm:max-w-md'>
-      <UnstyledLink href='https://schematics.its.ac.id/'>
+      <UnstyledLink openNewTab={false} href='https://schematics.its.ac.id/'>
         <img
           className='w-1/2 mx-auto'
           src={`${process.env.PUBLIC_URL}/images/logo/colored-title.png`}
