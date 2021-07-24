e05e4419b534d132bba61b1db78f6f6d367f22e2
Theodorus Clarence <theodorusclarence@gmail.com>
2021-07-24 21:40:44 +0700

    feat: add openNewTab props to  UnstyledLink

--- src/components/UnstyledLink.jsx
+++ src/components/UnstyledLink.jsx
@@ -1,25 +1,34 @@
 import { Link } from 'react-router-dom';
 
-export default function UnstyledLink(props) {
-  const href = props.href;
+export default function UnstyledLink({
+  href,
+  className = '',
+  openNewTab,
+  children,
+  ...rest
+}) {
   const isInternalLink = href && (href.startsWith('/') || href.startsWith('#'));
 
-  if (isInternalLink) {
+  // If is internal link, and no override specified
+  if (isInternalLink && openNewTab === undefined) {
     return (
-      <Link to={href} className={`${props?.className}`}>
-        {props.children}
+      <Link to={href} className={className}>
+        {children}
       </Link>
     );
   }
 
+  // If no override then _blank
+  // if there is override then use the boolean
   return (
     <a
-      className={`${props?.className}`}
-      target='_blank'
+      href={href}
+      className={className}
+      target={openNewTab === undefined || openNewTab ? '_blank' : undefined}
       rel='noopener noreferrer'
-      {...props}
+      {...rest}
     >
-      {props.children}
+      {children}
     </a>
   );
 }
