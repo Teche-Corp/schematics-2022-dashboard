1dfa8479a61d84f7fdb9febc57f328e0483b5954
rizqitsani <rizqitsani@gmail.com>
2021-07-28 20:05:19 +0700

    feat: connect admin voucher list to backend

--- src/components/VoucherTable.jsx
+++ src/components/VoucherTable.jsx
@@ -1,167 +1,191 @@
-import { FaTimes } from 'react-icons/fa';
-import { AiOutlineEdit } from 'react-icons/ai';
+import { useGlobalFilter, usePagination, useTable } from 'react-table';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
 
-const voucher = [
-  {
-    kode: '67563',
-    keterangan: '-',
-    potongan: '10',
-    limit: '3',
-    mulai: '01 JAN 2021',
-    akhir: '31 JAN 2021',
-    status: 'Tidak Aktif',
-  },
-  {
-    kode: '73733',
-    keterangan: '-',
-    potongan: '15',
-    limit: '3',
-    mulai: '01 FEB 2021',
-    akhir: '28 FEB 2021',
-    status: 'Tidak Aktif',
-  },
-  {
-    kode: '39469',
-    keterangan: '-',
-    potongan: '50',
-    limit: '1',
-    mulai: '01 MAR 2021',
-    akhir: '31 MAR 2021',
-    status: 'Tidak Aktif',
-  },
-  {
-    kode: '13730',
-    keterangan: '-',
-    potongan: '20',
-    limit: '2',
-    mulai: '01 APR 2021',
-    akhir: '30 APR 2021',
-    status: 'Aktif',
-  },
-  {
-    kode: '22817',
-    keterangan: '-',
-    potongan: '25',
-    limit: '2',
-    mulai: '01 MEI 2021',
-    akhir: '31 MEI 2021',
-    status: 'Aktif',
-  },
-];
+import GlobalFilter from '@/components/GlobalFilter';
 
-export default function VoucherTable() {
+import { classNames } from '@/lib/helper';
+
+export default function VoucherTable({ columns, data }) {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page,
+    preGlobalFilteredRows,
+    setGlobalFilter,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { globalFilter, pageIndex, pageSize },
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0 },
+    },
+    useGlobalFilter,
+    usePagination,
+  );
+
   return (
     <div className='flex flex-col'>
       <div className='-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
-        <div className='inline-block min-w-full py-2 align-middle sm:px-6 lg:px-10'>
-          <div className='overflow-hidden border-b border-gray-200 shadow rounded-lg'>
-            <table className='min-w-full divide-y divide-gray-200'>
-              <thead className='bg-gray-50'>
-                <tr>
-                  <th
-                    className='px-6 py-3 text-xs text-center font-medium tracking-wider text-left text-gray-500 uppercase'
-                    scope='col'
-                  >
-                    No
-                  </th>
-                  <th
-                    className='px-6 py-3 text-xs text-center font-medium tracking-wider text-left text-gray-500 uppercase'
-                    scope='col'
-                  >
-                    Kode
-                  </th>
-                  <th
-                    className='px-6 py-3 text-xs text-center font-medium tracking-wider text-left text-gray-500 uppercase'
-                    scope='col'
-                  >
-                    Keterangan
-                  </th>
-                  <th
-                    className='px-6 py-3 text-xs text-center font-medium tracking-wider text-left text-gray-500 uppercase'
-                    scope='col'
-                  >
-                    Potongan
-                  </th>
-                  <th
-                    className='px-6 py-3 text-xs text-center font-medium tracking-wider text-left text-gray-500 uppercase'
-                    scope='col'
-                  >
-                    Limit
-                  </th>
-                  <th
-                    className='px-6 py-3 text-xs text-center font-medium tracking-wider text-left text-gray-500 uppercase'
-                    scope='col'
-                  >
-                    Mulai Tgl
-                  </th>
-                  <th
-                    className='px-15 py-3 text-xs text-center font-medium tracking-wider text-left text-gray-500 uppercase'
-                    scope='col'
-                  >
-                    Berakhir Tgl
-                  </th>
-                  <th
-                    className='px-6 py-3 text-xs text-center font-medium tracking-wider text-left text-gray-500 uppercase'
-                    scope='col'
-                  >
-                    Status
-                  </th>
-                  <th
-                    className='px-6 py-3 text-xs text-center font-medium tracking-wider text-left text-gray-500 uppercase'
-                    scope='col'
-                  ></th>
-                </tr>
-              </thead>
-              <tbody>
-                {voucher.map((voucher, voucherIdx) => (
-                  <tr
-                    key={voucher.kode}
-                    className={voucherIdx % 2 === 0 ? 'bg-white' : 'bg-gray-50'}
-                  >
-                    <td className='px-6 py-4 text-center text-sm font-medium text-gray-900 whitespace-nowrap'>
-                      {voucherIdx + 1}
-                    </td>
-                    <td className='px-6 py-4 text-center text-sm font-medium text-gray-900 whitespace-nowrap'>
-                      {voucher.kode}
-                    </td>
-                    <td className='px-6 py-4 text-center text-sm text-gray-500 whitespace-nowrap'>
-                      {voucher.keterangan}
-                    </td>
-                    <td className='px-6 py-4 text-center text-sm text-gray-500 whitespace-nowrap'>
-                      {voucher.potongan}%
-                    </td>
-                    <td className='px-6 py-4 text-center text-sm text-gray-500 whitespace-nowrap'>
-                      {voucher.limit}
-                    </td>
-                    <td className='px-6 py-4 text-center text-sm text-gray-500 whitespace-nowrap'>
-                      {voucher.mulai}
-                    </td>
-                    <td className='px-6 py-4 text-center text-sm text-gray-500 whitespace-nowrap'>
-                      {voucher.akhir}
-                    </td>
-                    <td className='px-6 py-4 text-center text-sm text-gray-500 whitespace-nowrap'>
-                      <strong
-                        className={
-                          voucher.status === 'Aktif'
-                            ? 'text-green-400'
-                            : 'text-red-400'
-                        }
+        <div className='inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8'>
+          <GlobalFilter
+            preGlobalFilteredRows={preGlobalFilteredRows}
+            globalFilter={globalFilter}
+            setGlobalFilter={setGlobalFilter}
+          />
+          <div className='overflow-hidden border-b border-gray-200 shadow sm:rounded-lg'>
+            {data === [] ? (
+              <div className='text-center'>
+                <div className='py-4 text-sm text-gray-900'>
+                  Sedang menunggu data...
+                </div>
+              </div>
+            ) : (
+              <>
+                <table
+                  columns={columns}
+                  data={data}
+                  {...getTableProps()}
+                  className='min-w-full divide-y divide-gray-200'
+                >
+                  <thead className='bg-gray-50'>
+                    {headerGroups.map((headerGroup) => (
+                      <tr
+                        {...headerGroup.getHeaderGroupProps()}
+                        key={headerGroup.id}
                       >
-                        {voucher.status}
-                      </strong>
-                    </td>
-                    <td className='px-3 py-4 text-center text-sm text-gray-500 whitespace-nowrap'>
-                      <button className='mr-2'>
-                        <AiOutlineEdit />
-                      </button>
-                      <button>
-                        <FaTimes className='text-red-500' />
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+                        {headerGroup.headers.map((column) => (
+                          <th
+                            key={column.id}
+                            {...column.getHeaderProps()}
+                            className='px-6 py-3 text-xs font-bold tracking-wider text-left text-black uppercase'
+                          >
+                            {column.render('Header')}
+                          </th>
+                        ))}
+                      </tr>
+                    ))}
+                  </thead>
+                  <tbody {...getTableBodyProps()}>
+                    {page?.map((row) => {
+                      prepareRow(row);
+                      return (
+                        <tr
+                          {...row.getRowProps()}
+                          className={
+                            row.id % 2 === 0 ? 'bg-white' : 'bg-gray-100'
+                          }
+                          key={row.id}
+                        >
+                          {row?.cells?.map((cell) => {
+                            return (
+                              <td
+                                {...cell.getCellProps()}
+                                className='px-6 py-4 text-sm font-light text-gray-900 whitespace-nowrap '
+                                key={cell.id}
+                              >
+                                {cell.render('Cell')}
+                              </td>
+                            );
+                          })}
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </>
+            )}
+            <div className='flex items-center justify-between px-4 py-3 bg-white border-t border-gray-200 sm:px-6'>
+              <div className='flex flex-1 sm:hidden'>
+                <button
+                  onClick={() => previousPage()}
+                  disabled={!canPreviousPage}
+                  className={classNames(
+                    'relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500  border border-gray-300 rounded-l-md',
+                    canPreviousPage
+                      ? 'bg-white hover:bg-gray-50'
+                      : 'bg-gray-200',
+                  )}
+                >
+                  Previous
+                </button>
+                <button
+                  onClick={() => nextPage()}
+                  disabled={!canNextPage}
+                  className={classNames(
+                    'relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 border border-gray-300 rounded-r-md',
+                    canNextPage ? 'bg-white hover:bg-gray-50' : 'bg-gray-200',
+                  )}
+                >
+                  Next
+                </button>
+              </div>
+              <div className='hidden sm:flex-1 sm:flex sm:items-center sm:justify-between'>
+                <div>
+                  <p className='text-sm text-gray-700'>
+                    Menampilkan halaman
+                    <span className='font-medium'> {pageIndex + 1} </span>
+                    dari
+                    <span className='font-medium'> {pageOptions.length} </span>
+                    halaman
+                  </p>
+                </div>
+                <div>
+                  <nav
+                    className='relative z-0 inline-flex -space-x-px rounded-md shadow-sm'
+                    aria-label='Pagination'
+                  >
+                    <button
+                      onClick={() => previousPage()}
+                      disabled={!canPreviousPage}
+                      className={classNames(
+                        'relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500  border border-gray-300 rounded-l-md',
+                        canPreviousPage
+                          ? 'bg-white hover:bg-gray-50'
+                          : 'bg-gray-200',
+                      )}
+                    >
+                      <span className='sr-only'>Previous</span>
+                      <HiChevronLeft className='w-5 h-5' aria-hidden='true' />
+                    </button>
+
+                    <button
+                      onClick={() => nextPage()}
+                      disabled={!canNextPage}
+                      className={classNames(
+                        'relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 border border-gray-300 rounded-r-md',
+                        canNextPage
+                          ? 'bg-white hover:bg-gray-50'
+                          : 'bg-gray-200',
+                      )}
+                    >
+                      <span className='sr-only'>Next</span>
+                      <HiChevronRight className='w-5 h-5' aria-hidden='true' />
+                    </button>
+                  </nav>
+                </div>
+              </div>
+              <br />
+              <div>
+                <select
+                  value={pageSize}
+                  onChange={(e) => setPageSize(Number(e.target.value))}
+                >
+                  {[1, 10, 50].map((pageSize) => (
+                    <option key={pageSize} value={pageSize}>
+                      Show {pageSize}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/admin/voucher/Voucher.jsx
+++ src/pages/admin/voucher/Voucher.jsx
@@ -1,71 +1,134 @@
-import DashboardAdminShell from '@/layout/DashboardAdminShell';
-import VoucherTable from '@/components/VoucherTable';
+import { useEffect, useMemo, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { HiCheckCircle, HiOfficeBuilding } from 'react-icons/hi';
+import axios from 'axios';
+
+import { ImSpinner } from 'react-icons/im';
+
+import DashboardAdminShell from '@/layout/DashboardAdminShell';
+
+import { bearerToken } from '@/lib/helper';
+import VoucherTable from '@/components/VoucherTable';
 
 export default function Admin() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getVoucherData = async () => {
+      try {
+        const res = await axios.post(
+          'voucher/list',
+          {},
+          {
+            headers: { ...bearerToken() },
+          },
+        );
+
+        setData(res.data.data);
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getVoucherData();
+  }, []);
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'No',
+        Cell: (d) => {
+          return d.row.index + 1;
+        },
+      },
+      {
+        Header: 'Kode',
+        accessor: 'kode_voucher',
+      },
+      {
+        Header: 'Keterangan',
+        accessor: 'keterangan',
+      },
+      {
+        Header: 'Potongan',
+        accessor: 'potongan_persen',
+      },
+      {
+        Header: 'Limit',
+        accessor: 'limit_jumlah',
+      },
+      {
+        Header: 'Tanggal Mulai',
+        accessor: 'tanggal_mulai',
+      },
+      {
+        Header: 'Tanggal Berakhir',
+        accessor: 'tanggal_berakhir',
+      },
+      {
+        Header: 'Status',
+        accessor: (d) => {
+          if (d.is_active) {
+            <span className='text-green-500'>Aktif</span>;
+          }
+          return <span className='text-red-500'>Tidak</span>;
+        },
+      },
+      {
+        Header: 'Edit',
+        accessor: (d) => {
+          return (
+            <Link
+              className='font-bold text-nlc'
+              id={d?.kode_voucher}
+              to={{
+                pathname: `/admin/event/sch-nlc/user/${Number(
+                  d?.kode_voucher,
+                )}/edit`,
+              }}
+            >
+              Edit
+            </Link>
+          );
+        },
+      },
+    ],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
+  );
+
   return (
     <DashboardAdminShell>
-      <main className='relative z-0 flex-1 pb-8 overflow-y-auto'>
-        {/* Page header */}
-        <div className='bg-white shadow'>
-          <div className='px-4 sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8'>
-            <div className='py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-gray-200'>
-              <div className='flex-1 min-w-0'>
-                <div className='flex items-center'>
-                  <div>
-                    <div className='flex items-center'>
-                      <h1 className='text-2xl font-bold leading-7 text-gray-900 md:ml-3 sm:leading-9 sm:truncate'>
-                        Hello, Admin Schematics 2021
-                      </h1>
-                    </div>
-                    <dl className='flex flex-col mt-6 sm:ml-3 sm:mt-1 sm:flex-row sm:flex-wrap'>
-                      <dt className='sr-only'>Company</dt>
-                      <dd className='flex items-center text-sm font-medium text-gray-500 capitalize sm:mr-6'>
-                        <HiOfficeBuilding
-                          className='flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400'
-                          aria-hidden='true'
-                        />
-                        Institut Teknologi Sepuluh Nopember
-                      </dd>
-                      <dt className='sr-only'>Account status</dt>
-                      <dd className='flex items-center mt-3 text-sm font-medium text-gray-500 capitalize sm:mr-6 sm:mt-0'>
-                        <HiCheckCircle
-                          className='flex-shrink-0 mr-1.5 h-5 w-5 text-green-400'
-                          aria-hidden='true'
-                        />
-                        Akun Admin
-                      </dd>
-                    </dl>
-                  </div>
-                </div>
+      <main className='relative z-0 flex-1 pb-8 overflow-y-auto border-t'>
+        <div className='mt-8 '>
+          <div className='max-w-6xl px-4 mx-auto space-y-3 sm:px-6'>
+            <div className='flex items-center'>
+              <h2 className='max-w-6xl text-lg font-medium leading-6 text-gray-900'>
+                Daftar Voucher
+              </h2>
+              <div className=''>
+                <Link
+                  to='/admin/voucher/add'
+                  className='inline-flex justify-center px-4 py-2 ml-3 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-dark-700 hover:bg-dark-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-dark-400'
+                >
+                  Tambah Voucher
+                </Link>
               </div>
             </div>
-          </div>
-        </div>
 
-        <div className='mt-8'>
-          <div className='px-4 pb-16 sm:px-6 md:px-0'>
-            <div className='max-w-6xl px-4 mx-auto space-y-3 sm:px-6 lg:px-6'>
-              <div className='flex flex-wrap content-start'>
-                <h2 className='max-w-6xl pl-2 text-lg font-medium leading-6 text-gray-900 mt-7 '>
-                  Daftar Voucher
-                </h2>
-                <div className='pt-5'>
-                  <div className='flex justify-end'>
-                    <Link
-                      to='/admin/voucher/add'
-                      className='inline-flex justify-center px-4 py-2 ml-3 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-dark-700 hover:bg-dark-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-dark-400'
-                    >
-                      Tambah Voucher
-                    </Link>
-                  </div>
+            {loading ? (
+              <div className='flex justify-center py-10 mt-4'>
+                <div>
+                  <ImSpinner className='mx-auto mb-3 text-gray-700 w-7 h-7 animate-spin' />
+                  <p>Sedang menunggu data...</p>
                 </div>
               </div>
-              <div className='grid grid-cols-1 mx-2 sm:mr-8 lg:mx-0 mt-6 gap-y-1'>
-                <VoucherTable />
-              </div>
-            </div>
+            ) : (
+              <VoucherTable columns={columns} data={data} />
+            )}
           </div>
         </div>
       </main>
