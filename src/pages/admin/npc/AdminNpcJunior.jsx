2fd3ac749a658c92397733bb4438221892c362d6
christhopermarcelino <christhopermarcelino@gmail.com>
2021-07-30 19:25:10 +0700

    fix: wrong team_id on edit link

--- src/pages/admin/npc/AdminNpcJunior.jsx
+++ src/pages/admin/npc/AdminNpcJunior.jsx
@@ -135,9 +135,9 @@ export default function AdminNpcJunior() {
               className='font-bold text-npc'
               id={d?.team_id}
               to={{
-                pathname: `/admin/event/sch-npc/junior/user/${
-                  Number(d?.team_id) + 1
-                }/edit`,
+                pathname: `/admin/event/sch-npc/junior/user/${Number(
+                  d?.team_id,
+                )}/edit`,
               }}
             >
               Edit
