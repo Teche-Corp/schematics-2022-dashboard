82a0ac0cc5a5a6efd8ca946da03b053139c73f38
rizqitsani <rizqitsani@gmail.com>
2021-08-12 14:46:20 +0700

    feat: change create ticket flow

--- src/pages/nst/EventNST.jsx
+++ src/pages/nst/EventNST.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import axios from 'axios';
 import toast from 'react-hot-toast';
-import { useHistory } from 'react-router';
 
 import { HiClipboardCheck, HiOutlineClock } from 'react-icons/hi';
 
@@ -38,8 +37,6 @@ export default function EventNST() {
   const dispatch = useTeamDispatch();
 
   const { user } = useAuthState();
-
-  const history = useHistory();
 
   const teamLoaded = Boolean(nst);
 
@@ -90,7 +87,6 @@ export default function EventNST() {
         )
         .then((res) => {
           dispatch('STORE_NST', res.data.data);
-          history.go(0);
         }),
       {
         ...defaultToastMessage,
@@ -108,29 +104,7 @@ export default function EventNST() {
         <div className='relative max-w-4xl mx-auto md:px-8 xl:px-0'>
           <div className='pt-10 pb-16'>
             {/* If in AuthContext has teamId, then show the team detail page, and show skeleton */}
-            {!user?.nst_ticket ? (
-              <div className='px-4 text-center sm:px-6 md:px-0'>
-                <h1 className='text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl'>
-                  <span className='block xl:inline'>Schematics</span>{' '}
-                  <span className='block text-nst xl:inline'>NST</span>
-                </h1>
-                <p className='max-w-md mx-auto mt-3 text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl'>
-                  Schematics NST bertujuan untuk membagikan pengetahuan dan
-                  perkemabangan teknologi informasi terkini
-                </p>
-                <div className='max-w-md mx-auto mt-5 sm:flex sm:justify-center md:mt-8'>
-                  <div className='rounded-md shadow'>
-                    <button
-                      type='button'
-                      onClick={handleCreateTicket}
-                      className='flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white border border-transparent rounded-md shadow-sm bg-nst hover:bg-nst-400 md:py-4 md:text-lg md:px-10 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-nst-400'
-                    >
-                      Buat Tiket
-                    </button>
-                  </div>
-                </div>
-              </div>
-            ) : (
+            {user?.nst_ticket || nst ? (
               <>
                 <section className='px-4 space-y-10 sm:px-6 md:px-0'>
                   <h2 className='text-3xl font-extrabold tracking-tight text-center text-gray-900 sm:text-4xl md:text-5xl'>
@@ -154,6 +128,28 @@ export default function EventNST() {
                   </div>
                 </section>
               </>
+            ) : (
+              <div className='px-4 text-center sm:px-6 md:px-0'>
+                <h1 className='text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl'>
+                  <span className='block xl:inline'>Schematics</span>{' '}
+                  <span className='block text-nst xl:inline'>NST</span>
+                </h1>
+                <p className='max-w-md mx-auto mt-3 text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl'>
+                  Schematics NST bertujuan untuk membagikan pengetahuan dan
+                  perkemabangan teknologi informasi terkini
+                </p>
+                <div className='max-w-md mx-auto mt-5 sm:flex sm:justify-center md:mt-8'>
+                  <div className='rounded-md shadow'>
+                    <button
+                      type='button'
+                      onClick={handleCreateTicket}
+                      className='flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white border border-transparent rounded-md shadow-sm bg-nst hover:bg-nst-400 md:py-4 md:text-lg md:px-10 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-nst-400'
+                    >
+                      Buat Tiket
+                    </button>
+                  </div>
+                </div>
+              </div>
             )}
           </div>
         </div>
