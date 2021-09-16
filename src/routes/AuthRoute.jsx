e1d39167d581cd0d32b0adc90d61cf5787cc3cbb
rizqitsani <rizqitsani@gmail.com>
2021-09-16 21:01:11 +0700

    feat: add redirect if authenticated

--- src/routes/AuthRoute.jsx
+++ src/routes/AuthRoute.jsx
@@ -3,9 +3,25 @@ import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { ImSpinner8 } from 'react-icons/im';
 import { useAuthState } from '@/contexts/AuthContext';
+import useQuery from '@/hooks/useQuery';
+
+const apiUrl =
+  process.env.NODE_ENV === 'production' &&
+  process.env.PUBLIC_URL === '/dashboard'
+    ? 'https://schematics.its.ac.id/api'
+    : 'https://schematics-webkes-backend-dev.herokuapp.com/api';
 
 const AuthRoute = ({ component: Component, ...rest }) => {
   const { authenticated, loading } = useAuthState();
+
+  const query = useQuery();
+  const redirectTo = query.get('redirect_to');
+
+  if (redirectTo && rest.path === '/signin') {
+    window.location.replace(
+      `${apiUrl}/auth/login/redirect?redirect_to=${encodeURI(redirectTo)}`,
+    );
+  }
 
   return (
     <Route
@@ -16,7 +32,7 @@ const AuthRoute = ({ component: Component, ...rest }) => {
             <ImSpinner8 className='mb-2 text-4xl animate-spin' />
             <p>Loading...</p>
           </div>
-        ) : authenticated ? (
+        ) : authenticated && !redirectTo ? (
           <Redirect to={'/my'} />
         ) : (
           <Component {...props} />
