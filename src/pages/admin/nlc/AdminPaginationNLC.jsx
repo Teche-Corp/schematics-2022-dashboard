494821cb8b99da7c3c1a32b359720c20d0920829
Maula Izza Azizi <azizi.maula@gmail.com>
2022-07-28 12:39:37 +0700

    fix: fetch table row -nlc

--- src/pages/admin/nlc/AdminPaginationNLC.jsx
+++ src/pages/admin/nlc/AdminPaginationNLC.jsx
@@ -7,34 +7,38 @@ import axios from 'axios';
 import { useForm } from 'react-hook-form';
 import GetPembayaranNLC from '@/components/Infrastructure/GetPembayaranNLC';
 import useSWR from 'swr';
+import Loading from '@/components/Loading';
 
 const column = [
   {
     // field: "front_end_position.name.full_name",
     // use: "Position",
-    field: 'front_end_position.name.full_name',
+    field: 'team.name.lead_name',
     use: 'Nama Ketua Tim',
     //Will not be used in search filtering
-    //  use_in_search:false
+    use_in_search: true,
   },
   {
-    field: 'name',
+    field: 'id',
+    use: 'pembayaran id',
+    use_in_display: false,
+  },
+  {
+    field: 'team.name.bank',
     use: 'Nama Bank',
   },
   {
     field: 'status',
     use: 'Status Pembayaran',
-
     //Will not be displayed in the table
     use_in_display: true,
   },
   {
-    field: 'team',
+    field: 'team.name.name_team',
     use: 'Nama Tim',
+    use_in_search: true,
   },
 ];
-
-const row = [];
 
 const tableStyling = {
   base_bg_color: 'bg-nlc',
@@ -72,28 +76,60 @@ const tableStyling = {
 
 const AdminPagination = () => {
   const history = useHistory();
-
+  const [perPage] = useState(10);
+  const [loadingData, setLoadingData] = useState(true);
+  const [pageCount, setPageCount] = useState(0);
   const methods = useForm();
   const { control, handleSubmit } = methods;
   const formData = new FormData();
 
-  axios.defaults.baseURL = `https://schematics.its.ac.id:8081/api`;
+  // axios.defaults.baseURL = `https://schematics.its.ac.id:8081/api`;
+
+  // const fetcher = (url) => {
+  //   axios.get(url).then((res) => console.log(res))
+  // }
+
   const page = 1;
   const per_page = 10;
+  const url = `/admin_get_list_pembayaran_nlc?page=${page}&per_page=${per_page}`;
   const { data, error } = useSWR(
-    `/admin_get_list_pembayaran_nlc?page=${page}&per_page=${per_page}`,
+    url,
     {
       headers: { ...bearerToken() },
     },
+    // fetcher(url)
   );
-  console.log('datanya : ', data);
+  console.log('datanya : ', data?.data?.data_per_page);
   if (!data) console.log(error);
 
+  const [row, setRow] = useState(undefined);
+
+  useEffect(() => {
+    if (data) {
+      let rowMap = data?.data?.data_per_page?.map((payment) => {
+        return {
+          id: payment?.pembayaran_id,
+          team: {
+            name: {
+              lead_name: payment.nama_ketua,
+              bank: payment.nama_bank,
+              name_team: payment.nama_tim,
+            },
+          },
+          status: payment.status_pembayaran,
+        };
+      });
+      console.log('row map :', rowMap);
+      setRow(rowMap);
+    }
+  }, [data]);
+  if (!data || !row) {
+    return <Loading />;
+  }
   return (
     <div style={{ padding: '20px' }}>
       <Table
         columns={column}
-        // rows={handleData}
         rows={row}
         per_page={5}
         table_header='Daftar Pembayaran Schematics NLC'
