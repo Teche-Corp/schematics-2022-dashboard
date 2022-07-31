9dfdebb03e82adb5828ce22f653942c69f28e661
Robby Pambudi <robby.pambudi10@gmail.com>
2022-07-31 19:23:38 +0700

    feat: add select input for payment page

---
+++ src/components/SelectInputPayment.jsx
@@ -0,0 +1,70 @@
+import { useFormContext } from 'react-hook-form';
+import { HiExclamationCircle } from 'react-icons/hi';
+
+import { classNames } from '@/lib/helper';
+
+export default function SelectInputPayment({
+  label,
+  helperText = '',
+  id,
+  placeholder = '',
+  readOnly = false,
+  options = [],
+  validation,
+  disabled = false,
+}) {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
+  return (
+    <div className='mb-3'>
+      <label
+        htmlFor={id}
+        className='block text-sm font-normal text-white font-tertiary'
+      >
+        {label}
+      </label>
+      <div className='relative mt-1'>
+        <select
+          {...register(id, validation)}
+          name={id}
+          id={id}
+          disabled={disabled}
+          className={classNames(
+            readOnly || disabled === true ? 'bg-gray-100' : '',
+            errors[id]
+              ? 'focus:ring-red-500 border-red-500 focus:border-red-500'
+              : 'focus:ring-dark-400 focus:border-dark-400',
+            'block w-full border-gray-300 rounded-md shadow-sm  sm:text-sm`',
+          )}
+          aria-describedby={id}
+        >
+          <option value='' disabled hidden>
+            {placeholder}
+          </option>
+          {options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.text}
+            </option>
+          ))}
+        </select>
+
+        {errors[id] && (
+          <div className='absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none'>
+            <HiExclamationCircle className='text-xl text-red-500' />
+          </div>
+        )}
+      </div>
+      <div className='mt-1'>
+        {helperText !== '' && (
+          <p className='text-xs text-gray-500'>{helperText}</p>
+        )}
+        {errors[id] && (
+          <span className='text-sm text-red-500'>{errors[id].message}</span>
+        )}
+      </div>
+    </div>
+  );
+}
