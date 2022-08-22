b93dd7aae5afccf2681e8f149090a8bc262337a9
akmalf06 <akmaldavinci06@gmail.com>
2022-08-22 20:06:04 +0700

    chore: nlc button promo

--- src/pages/nlc/CreateTeamKetua.jsx
+++ src/pages/nlc/CreateTeamKetua.jsx
@@ -326,7 +326,7 @@ export default function CreateTeamKetua() {
                 <button
                   type='button'
                   onClick={handleCheckKode}
-                  className='flex items-center justify-center w-full px-4 py-2 text-sm font-medium border border-transparent rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 hover:bg-light-700 font-primary bg-npc'
+                  className='flex items-center justify-center w-full px-4 py-2 text-sm font-medium border border-transparent rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 hover:bg-light-700 font-primary bg-nlc'
                 >
                   Cek Kode
                 </button>
