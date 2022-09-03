e26c52b4faea94dcc5c6c3928018ce3fff2542dd
akmalf06 <akmaldavinci06@gmail.com>
2022-09-03 12:00:48 +0700

    feat: disable nst beli 5 gratis 1

--- src/pages/nst/NSTregister.jsx
+++ src/pages/nst/NSTregister.jsx
@@ -86,10 +86,10 @@ export default function ReevaRegister() {
         <p className='md:text-5xl text-3xl font-primary text-center text-white'>
           Data Pendaftaran
         </p>
-        <p className='md:text-2xl text-lg font-primary text-center text-white mt-4'>
+        {/* <p className='md:text-2xl text-lg font-primary text-center text-white mt-4'>
           <span className='text-nst'>Gratis 1 tiket</span> untuk pembelian{' '}
           <span className='text-nst'>5 tiket</span> secara langsung!
-        </p>
+        </p> */}
         <FormProvider {...methods}>
           <form
             onSubmit={handleSubmit(handleReevaRegister)}
