b478adf3cc5044f8e1dd565327521851c1538236
Tigo S Yoga <tigo.yoga@gmail.com>
2022-10-09 23:24:41 +0700

    feat: add logo event

--- src/components/DashboardEventCard.jsx
+++ src/components/DashboardEventCard.jsx
@@ -3,18 +3,7 @@ import React from 'react';
 function DashboardEventCard({ textpicture, name, img1, img2, status }) {
   return (
     <>
-      <div className='w-full md:w-1/5 h-[360px] py-6 bg-white rounded-xl relative'>
-        <img
-          className='w-11/12 mx-auto mb-4'
-          src='/images/schematics-dashboard.png'
-          alt=''
-        />
-        <img src={textpicture} alt='text-nlc' className='mx-auto' />
-        <p className='mt-1 text-center text-xs font-semibold px-4'>{name}</p>
-        <div className='w-full h-52 relative pt-4'>
-          <img src={img1} alt='assetnlc1' className='absolute left-0' />
-          <img src={img2} alt='assetnlc2' className='absolute right-0' />
-        </div>
+      <div className='w-full min-h-26 py-6 bg-white rounded-xl relative'>
         <p
           className={`px-4 font-bold absolute bottom-6 ${
             status === 'Mengikuti' ? 'text-blue-600' : 'text-red-500'
